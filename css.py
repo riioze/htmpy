80b949e027318bae132074df620f2896941291a8
riioze <61903425+riioze@users.noreply.github.com>
2021-02-15 11:33:56 +0100

    0.2.3
    -the rule with the same declarator are now grouped in styleSheet.render()
    -we can add a batch of rules in a sheet with styleSheet.addList(listr)

--- css.py
+++ css.py
@@ -11,15 +11,34 @@ class styleSheet:
 	def __init__(self):
 		"""creates the sheet"""
 		self.rules = []
+	
+	def sort(self):
+		sortedDict = {}
+		for rule in self.rules:
+			if (rule.tag,rule.id,rule._class) in sortedDict.keys():
+				sortedDict[(rule.tag,rule.id,rule._class)].append((rule.property,rule.value))
+			else:
+				sortedDict[(rule.tag,rule.id,rule._class)] = [(rule.property,rule.value)]
+		return sortedDict
+
 	def add(self,rule):
 		""" to add a rule to the sheet """
 		self.rules.append(rule)
 
+	def addList(self,listr):
+		"""to add a list of rule"""
+		for rule in listr:
+			self.add(rule)
+
 	def render(self):
 		""" to render the rules (one by one but it will be grouped later) """
 		r = ''
-		for rule in self.rules:
-			r+=rule.render()+'\n'
+		
+		ruleDict = self.sort()
+
+		for Selector,declaration in ruleDict.items():
+			r+=rule.renderBlock(Selector,declaration)
+
 		return r
 	
 	def save(self,path):
@@ -58,15 +77,35 @@ class rule:
 		r+=str(self.value)+';\n}'
 		return r
 
+	def renderBlock(Selector,declarations):
+		"""to render a block with a batch of declaration"""
+		tag,_id,_class = Selector
+		r = ''
+		if tag:
+			r+=tag+' '
+		if _id:
+			r+='#'+_id+' '
+		if _class:
+			r+='.'+_class
+		r+='\n{\n'
+
+		for _property,value in declarations:
+			r+='\t'+_property+' : '+value+';\n'
+
+		r+='}\n'
+		return r
+
+
 
 
 
 if __name__=='__main__':
 	s=styleSheet()
-	a = rule('border','blue','a','test','test')
+	a = rule('border','blue','a','test')
 	b = rule('height','50%','p','p1','allp')
-	s.add(a)
-	s.add(b)
+	c = rule('color','green','a','test')
+	s.addList([a,b,c])
 	print(s.render())
+	print(s.sort())
 	s.save('style.css')
 
